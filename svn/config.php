7e11cef3ef97a63b64046a7103b82b034d5748b6
iceiceice <781256912@qq.com>
2016-05-23 10:41:59 +0800

    file

--- svn/config.php
+++ svn/config.php
@@ -2,7 +2,7 @@
 define('ROOT_DIR',str_replace('\\','/',realpath(dirname(__FILE__).'/../')));
 define('UPLOAD_FILE', ROOT_DIR . '/file/');
 define('SVN_FILE', ROOT_DIR . '/svn/');
-define('CONNECT_IP','111.111.111.111');
+define('CONNECT_IP','123.123.123.123');
 define('PREFIX_PATH','/tmp/');
 define('LOG_PATH', ROOT_DIR . '/log/');
 define('PUBLIC_KEY',ROOT_DIR . '/key/Identity.pub');
